3dd186a6da4ede037b5da576bcea346bfd94f16c
lourdespamela <lourdespjuarez@gmail.com>
2021-12-02 18:56:43 -0600

    Added scroll interaction in js and hover over nav bar

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-    // $("#project-tour-button").click(function() { $('html, body').animate({
-    //     scrollTop: $("#project-tour").offset().top
-    // }, 2000);});
+// attempt 1 $("#project-scroll").click(function() {
+//     document.getElementById("scroll-landing").scrollIntoView();
+// });
+
+// attempt 2 $(document).ready(function () {  
+//     $('#project-scroll').click(function(event){
+//       $('#scroll-landing').scrollIntoView(true);
+//     });
+// });
+
+// attempt 3 $(document).ready(function () {
+//     $('#project-scroll').click(function (event) {
+//       event.preventDefault();
+//       scrollToElement($('#scroll-landing'));
+//     });
+  
+//     function scrollToElement(elem) {
+//       $('body').animate({
+//           scrollTop: elem.offset().top
+//       }, 2000);
+//     }
+//   });
+
+function down() {
+    document.getElementById("scroll-landing").scrollIntoView();
+  }
