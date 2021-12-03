4edb9ab3d646b328cca0532272ec140df2791867
lourdespamela <lourdespjuarez@gmail.com>
2021-12-02 18:18:23 -0600

    Added projects back to nav

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-<script src="js/index.js">
-</script>
-
+    // $("#project-tour-button").click(function() { $('html, body').animate({
+    //     scrollTop: $("#project-tour").offset().top
+    // }, 2000);});
